811de1372b8b8e24d29dd08d2bbde82d34edeb58
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-05-01 20:14:49 +0200

    Fix Error

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
-const hours = date.getHoyurs();
+const hours = date.getHours();
 const minutes = date.getMinutes();
  
 const formattedDate = `${day}-${month}-${year}`;
@@ -15,6 +15,16 @@ dateOptionsSelectElement.addEventListener("change", () => {
     switch  (dateOptionsSelectElement.value) {
         case "yyyy-mm-dd":
             currentDateParagraph.textContent = formattedDate
+            .split("-")
+            .reverse()
+            .join("-");
+        break;
+        case "mm-dd-yyyy-h-mm":
+            currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+        break;
+        default:
+            currentDateParagraph.textContent = formattedDate
+    
 
     }
 }); 
